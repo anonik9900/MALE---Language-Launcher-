5c1dceeb4603c52563c8c4dc770974f700e40d14
DeVeL_AnOnIk <27363518+anonik9900@users.noreply.github.com>
2021-06-09 22:19:46 +0200

    Launcher src (visualBasic Only)

---
+++ Mass Effect LE Launcher/Form1.Designer.vb
@@ -0,0 +1,134 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.Mass_Effect_LE_Launcher.My.Resources.Resources.wallpaper
+        Me.PictureBox1.Location = New System.Drawing.Point(0, -6)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(1056, 663)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.White
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 36.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.SystemColors.Control
+        Me.Label1.Image = Global.Mass_Effect_LE_Launcher.My.Resources.Resources.wallpaper
+        Me.Label1.ImageAlign = System.Drawing.ContentAlignment.TopCenter
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(1043, 55)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Mass Effect LE - Language Launcher by Anonik" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(440, 491)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(200, 52)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "Mass Effect  LE  - Lang Launcher" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"Italian", "English", "French", "German", "Polish", "Russian", "Japanese", "Spanish"})
+        Me.ComboBox1.Location = New System.Drawing.Point(440, 432)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(200, 21)
+        Me.ComboBox1.TabIndex = 3
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.Label2.Image = Global.Mass_Effect_LE_Launcher.My.Resources.Resources.wallpaper
+        Me.Label2.ImageAlign = System.Drawing.ContentAlignment.MiddleRight
+        Me.Label2.Location = New System.Drawing.Point(467, 368)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(148, 31)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Languages"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.Label3.Image = Global.Mass_Effect_LE_Launcher.My.Resources.Resources.wallpaper
+        Me.Label3.Location = New System.Drawing.Point(421, 614)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(227, 29)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Credtis: Anonik9900"
+        Me.Label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1056, 642)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Mass Effect LE  Launcher - By Anonik"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents ComboBox1 As System.Windows.Forms.ComboBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+
+End Class
---
+++ Mass Effect LE Launcher/Form1.vb
@@ -0,0 +1,168 @@
+﻿Public Class Form1
+
+
+    Private Declare Auto Function GetPrivateProfileString Lib "kernel32.dll" (ByVal lpApplicationName As String, ByVal lpKeyName As String, ByVal lpDefault As String, ByVal lpReturnedString As String, ByVal nSize As Integer, ByVal lpFileName As String) As Integer
+    Private Declare Auto Function WritePrivateProfileString Lib "kernel32.dll" (ByVal lpApplicationName As String, ByVal lpKeyName As String, ByVal lpString As String, ByVal lpFileName As String) As Integer
+
+    Friend Function IniRead(ByVal Filename As String, ByVal Section As String, ByVal Key As String, Optional ByVal lpDefault As String = "", Optional ByVal bRaiseError As Boolean = False) As String
+
+        Dim RetVal As String = New String(" ", 255)
+
+        Dim LenResult As Integer
+
+        Dim ErrString As String
+
+        LenResult = GetPrivateProfileString(Section, Key, lpDefault, RetVal, RetVal.Length, Filename)
+
+        If LenResult = 0 AndAlso bRaiseError Then
+
+            If Not (System.IO.File.Exists(Filename)) Then
+
+                ErrString = "Impossibile aprire il file INI" & Filename
+
+            Else
+
+                ErrString = "La sezione o la chiave sono errate oppure l’accesso al file non è consentito"
+
+            End If
+
+            Throw New Exception(ErrString)
+
+        End If
+
+        Return RetVal.Substring(0, LenResult)
+
+    End Function
+
+    Friend Function IniWrite(ByVal Filename As String, ByVal Section As String, ByVal Key As String, ByVal Value As String, Optional ByVal bRaiseError As Boolean = False) As Boolean
+
+        Dim LenResult As Integer
+
+        Dim ErrString As String
+
+        LenResult = WritePrivateProfileString(Section, Key, Value, Filename)
+
+        If LenResult = 0 And bRaiseError Then
+
+            If Not (System.IO.File.Exists(Filename)) Then
+
+                ErrString = "Impossibile aprire il file INI" & Filename
+
+            Else
+
+                ErrString = "Accesso al file non consentito"
+
+            End If
+
+            Throw New Exception(ErrString)
+
+        End If
+
+        Return IIf(LenResult = 0, False, True)
+
+        End
+
+    End Function
+
+
+
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Label1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Label1.Click
+
+    End Sub
+
+
+
+    Private Sub CheckedListBox1_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs)
+
+    End Sub
+
+
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+
+
+
+
+
+
+        showForm1()
+
+
+
+
+
+
+
+
+    End Sub
+
+
+
+    Dim selectedItem1, selectedItem2, selectedItem3 As Object
+
+
+
+    
+
+
+    Private Sub showForm1()
+        selectedItem1 = ComboBox1.SelectedItem
+ 
+        If selectedItem1 Is Nothing Then
+            MsgBox("Error: Null Value Selected")
+            Exit Sub
+        End If
+
+        If ((selectedItem1.ToString() = "Italian")) Then
+            MsgBox("Lingua Selezionata: Italiano")
+            System.Diagnostics.Process.Start("C:\Games\Mass Effect Legendary Edition\Game\Launcher\lang-launcher\male-ita.exe")
+
+        ElseIf ((selectedItem1.ToString() = "English")) Then
+            MsgBox("Language Selected: English")
+            System.Diagnostics.Process.Start("C:\Games\Mass Effect Legendary Edition\Game\Launcher\lang-launcher\male-eng.exe")
+
+        ElseIf ((selectedItem1.ToString() = "French")) Then
+            MsgBox("Langue sélectionnée : Français")
+            System.Diagnostics.Process.Start("C:\Games\Mass Effect Legendary Edition\Game\Launcher\lang-launcher\male-fre.exe")
+
+        ElseIf ((selectedItem1.ToString() = "German")) Then
+            MsgBox("Ausgewählte Sprache: Deutsch")
+            System.Diagnostics.Process.Start("C:\Games\Mass Effect Legendary Edition\Game\Launcher\lang-launcher\male-deu.exe")
+
+        ElseIf ((selectedItem1.ToString() = "Spanish")) Then
+            MsgBox("Idioma seleccionado: Español")
+            System.Diagnostics.Process.Start("C:\Games\Mass Effect Legendary Edition\Game\Launcher\lang-launcher\male-esn.exe")
+
+        ElseIf ((selectedItem1.ToString() = "Polish")) Then
+            MsgBox("Wybrany język: Polski")
+            System.Diagnostics.Process.Start("C:\Games\Mass Effect Legendary Edition\Game\Launcher\lang-launcher\male-pol.exe")
+
+        ElseIf ((selectedItem1.ToString() = "Russian")) Then
+            MsgBox("Выбранный язык: Русский")
+            System.Diagnostics.Process.Start("C:\Games\Mass Effect Legendary Edition\Game\Launcher\lang-launcher\male-rus.exe")
+
+        ElseIf ((selectedItem1.ToString() = "Japanese")) Then
+            MsgBox("選択した言語: 日本語")
+            System.Diagnostics.Process.Start("C:\Games\Mass Effect Legendary Edition\Game\Launcher\lang-launcher\male-jpn.exe")
+
+        End If
+    End Sub
+
+    Private Sub Form2_Load(ByVal sender As Object, ByVal e As EventArgs) Handles MyBase.Load
+        ' your code to initialize items of ComboBox1, ComboBox2, ComboBox3
+    End Sub
+
+
+    Private Sub Label2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Label2.Click
+
+    End Sub
+
+    Private Sub Label3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Label3.Click
+        System.Diagnostics.Process.Start("chrome.exe", "https://github.com/anonik9900")
+    End Sub
+End Class
